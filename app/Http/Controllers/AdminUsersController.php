e578e657b5690574bb7dab34d0e67261489716a2
mendrek <mendrek94@gmail.com>
2016-10-28 17:24:28 +0200

    users section working 100 percent except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -47,7 +48,17 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        $input = $request->all();
+        if($request->password ==''){
+
+            $input = trim($request->except('password'));
+
+        }else{
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
 
         if($file = $request->file('photo_id')){
 
@@ -60,11 +71,11 @@ class AdminUsersController extends Controller
             $input['photo_id'] = $photo->id;
         }
 
-        $input['password'] = bcrypt($request->password);
+
 
         User::create($input);
 
-       // return redirect('/admin/users');
+        return redirect('/admin/users');
     }
 
     /**
@@ -88,7 +99,11 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.edit');
+
+
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -98,9 +113,31 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+
+            $name = time(). $file->getClientOriginalName();
+
+            $file->move('images',$name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+        $input['password'] = bcrypt($request->password);
+
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,11 +3,15 @@
 @section('content')
 
     <h1>Users</h1>
+<div class="row">
 
+    <div class="col-md-12 col-lg-6">
+        <div class="table-responsive">
     <table class="table table-bordered table-hover">
         <thead>
             <tr>
                 <th>Id</th>
+                <th>Photo</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
@@ -23,6 +27,9 @@
 
                     <tr>
                         <td>{{$user->id}}</td>
+
+
+                        <td><img height=50 src="{{$user->photo ? $user->photo->file : 'No user '}}" alt=""></td>
                         <td>{{$user->name}}</td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->role->name}}</td>
@@ -37,4 +44,50 @@
                 @endif
         </tbody>
     </table>
+        </div>
+    </div>
+
+
+    <div class="col-md-12 col-lg-6">
+        <div class="table-responsive">
+        <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Photo</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Status</th>
+                <th>Created</th>
+                <th>Updated</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @if($users)
+                @foreach($users as $user)
+
+                    <tr>
+                        <td>{{$user->id}}</td>
+
+
+                        <td><img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+                        <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->role->name}}</td>
+                        <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+                        <td>{{$user->created_at->diffForHumans()}}</td>
+                        <td>{{$user->updated_at->diffForHumans()}}</td>
+
+                    </tr>
+
+                @endforeach
+
+            @endif
+            </tbody>
+        </table>
+    </div>
+    </div>
+</div>
     @stop
